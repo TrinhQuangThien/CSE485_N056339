5fd04e8e3c0e4e94a2fbd5297c3743da16342b4a
TrinhQuangThien <45937277+TrinhQuangThien@users.noreply.github.com>
2019-01-02 23:39:52 +0700

    Add files via upload

---
+++ BTL/Sources/film/xman.php
@@ -0,0 +1,20 @@
+<?php
+include_once "page_header.php";
+echo "<br><br><br>";
+	echo "<div class='container'>";
+		echo "<div class='row'>";
+			echo "<div class='col-md-12'>";
+				echo "<article>";
+					echo "<h2 class='header-film'>Trailer</h2>";
+					echo "<video width='650' height='400' controls>";
+						echo "<source src='media/xman.mp4' type='video/mp4'>";
+							echo "<source src='media/boomboo.ogg' type='video/ogg'>";
+								echo "Your browser does not support the video tag.";
+							echo "</video>";
+						echo "</article>";
+					echo "</div>";
+				echo "</div>";
+			echo "</div>";
+
+include_once "page_footer.php";
+?>
